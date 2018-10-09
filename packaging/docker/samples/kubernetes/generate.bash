1a30af8084cc25cfc079921ab50220ea74ba7d20
John Brownlee <john_brownlee@apple.com>
2018-10-09 13:56:54 -0700

    Makes the coordinators in the kubernetes sample specific to each cluster.

--- packaging/docker/samples/kubernetes/generate.bash
+++ packaging/docker/samples/kubernetes/generate.bash
@@ -2,12 +2,16 @@
 
 script_dir=$(dirname $0)
 if [[ "$1" == "coordinators" ]]; then
-	count=$2
-	: > $script_dir/config/coordinators.yaml
+	cluster_name=$2
+	count=$3
+	coordinator_path=$script_dir/config/$cluster_name-coordinators.yaml
+	: > $coordinator_path
 	for index in $(seq 1 $count); do
-		sed -e "s/\$index/$index/g" $script_dir/config_templates/coordinators_template.yaml >> $script_dir/config/coordinators.yaml
+		sed -e "s/\$index/$index/g" $script_dir/config_templates/coordinators_template.yaml \
+			| sed -e "s/\$cluster_name/$cluster_name/g" \
+			>> $coordinator_path
 	done
-	echo "Generated $script_dir/config/coordinators.yaml"
+	echo "Generated $coordinator_path"
 elif [[ "$1" == "cluster" ]]; then
 	cluster_name=$2
 	replica_count=$3
@@ -18,7 +22,7 @@ elif [[ "$1" == "cluster" ]]; then
 	cluster_name_sanitized=$(echo $cluster_name | sed -e 's/[^0-9a-zA-z]//g')
 	cluster_file="$cluster_name_sanitized:init@"
 	for index in $(seq 1 $coordinator_count); do
-		coordinator_ip=$(kubectl get services/fdb-coordinators-$index -o go-template='{{(index .spec.clusterIP)}}')
+		coordinator_ip=$(kubectl get services/$cluster_name-coordinator-$index -o go-template='{{(index .spec.clusterIP)}}')
 		cluster_file="$cluster_file$coordinator_ip:4500"
 		if [[ $index -ne $coordinator_count ]]; then
 			cluster_file="$cluster_file,"
